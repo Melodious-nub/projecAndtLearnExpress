1389092700175408c181f4b46db2fc962072b9d5
Melodious-nub <shawon.taluckder1@gmail.com>
2023-05-13 23:53:07 +0600

    update with public folder

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+// this is a path import for accessing html files and folder
 const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+// this is a for joining public folder into express
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/hello', (req, res) => {
   res.send('Hello express!')
 })
 
@@ -15,6 +19,7 @@ app.get('/temp', (req, res) => {
 app.get('/json', (req, res) => {
   // res.send('this is about page!')
   // res.sendFile(path.join(__dirname, 'index.html'))
+  res.json({name: 'kashem', age: '16', height: '10'})
 })
 
 app.listen(port, () => {
